ef48d5f01ca1d902b583defa77abd8306fa9d370
Apoorv Kumar <96113133+SoftGenics@users.noreply.github.com>
2022-03-18 17:34:23 +0530

    Update Products.js

--- src/Components/Pages/Products.js
+++ src/Components/Pages/Products.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Row, Col, Card } from 'react-bootstrap'
 import Footer from '../Footer/Footer'
+import { Link } from 'react-router-dom'
 
 function Products() {
   return (
